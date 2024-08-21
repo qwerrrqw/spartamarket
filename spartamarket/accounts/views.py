abb8dea6804935907524f46cd032c4c16de87884
qwerrrqw <latiger95@gmail.com>
2024-08-21 18:02:41 +0900

    commit
    로그인, 로그아웃 기능 추가

--- spartamarket/accounts/views.py
+++ spartamarket/accounts/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 
 
 def login(request):
-    
-    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        if form.is_vaild():
+            auth_login(request, form.get_user())
+            return redirect("index")
+    else:
+        form = AuthenticationForm()
     context = {"form": form}
     return render(request, "accounts/login.html", context)
 
+def logout(request):
+    if request.method == "POST":
+        auth_logout(request)
+        return redirect("index")
+    
+def index(request):
+    return render(request, "accounts/index.html")
