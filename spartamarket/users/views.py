d05f19502193a6ec9c9018fd76c078b06b0c2530
qwerrrqw <latiger95@gmail.com>
2024-08-23 14:30:12 +0900

    commit
    users 앱에 프로필 기능 구현
    유저 이름
    가입일
    static image

--- spartamarket/users/views.py
+++ spartamarket/users/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def profile(request, pk):
-    pass
+    user = request.user
+    context = {"user": user}
+    return render(request, 'users/profile.html', context)
